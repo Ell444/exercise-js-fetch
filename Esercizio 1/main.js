35359f10179f3de5c47d2548abc7cc067334ce17
Laura Sirigu <laurasirigu43@gmail.com>
2023-11-26 18:31:19 +0100

    first commit || creato boilerplate

---
+++ Esercizio 1/main.js
@@ -0,0 +1,7 @@
+/*
+# Esercizio 1 - Posts
+Studiati come funzionano le API di https://jsonplaceholder.typicode.com/.
+Crea una pagina HTML in cui, dopo aver richiesto dei post all’API,
+vengono create delle card nel documento: per ogni card mostra titolo e contenuto.
+*/
+
