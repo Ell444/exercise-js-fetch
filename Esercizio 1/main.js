7105c0950d20013f520424034bdbfeee5dec1ada
Laura Sirigu <laurasirigu43@gmail.com>
2023-11-26 19:27:00 +0100

    feat || primo esercizio

--- Esercizio 1/main.js
+++ Esercizio 1/main.js
@@ -5,3 +5,24 @@ Crea una pagina HTML in cui, dopo aver richiesto dei post all’API,
 vengono create delle card nel documento: per ogni card mostra titolo e contenuto.
 */
 
+    window.addEventListener('load', () =>{
+
+    let posts;
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(obj => posts = obj)
+        .catch((error) => console.error(error))
+        .finally(() => {
+            
+
+                posts.forEach(({ title, body }) => {
+                     document.body.innerHTML += `
+                        <figure>
+                            <h3>${title}</h3>
+                            <p>${body}</p>
+                        </figure>
+                `;
+                });
+               
+            });
+        });
